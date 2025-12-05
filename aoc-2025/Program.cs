aa2c6f66a2a87c20001dbdcac83fa66793f8868d
Max <tumaobmaxjr@gmail.com>
2025-12-05 19:36:02 +0800

    Add Day 1 solution and input for AoC 2025
    Introduces the main program entry point and the Day 1 solution class for Advent of Code 2025. Includes input and sample files for Day 1, and logic to process dial rotations and count occurrences of position zero.

---
+++ aoc-2025/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please specify a day to run. Example: dotnet run 1");
+            return;
+        }
+
+        switch (args[0])
+        {
+            case "1":
+                D1? day1 = new D1();
+                day1.Solve();
+                break;
+            default:
+                Console.WriteLine($"Solutioon for day {args[0]} not found.");
+                break;
+        }
+    }
+}
---
+++ aoc-2025/d1/D1.cs
@@ -0,0 +1,44 @@
+public class D1
+{
+    private readonly string[] _lines;
+
+    public D1()
+    {
+        _lines = File.ReadAllLines(@"d1\input.txt");
+    }
+
+    public void Solve()
+    {
+        int currentPosition = 50;
+        Console.WriteLine($"The dial starts by pointing at {currentPosition}");
+
+        int count = 0;
+        for (int i = 0; i < _lines.Length; i++)
+        {
+            string line = _lines[i];
+            int val = int.Parse(line.Substring(1)); // remove L/R
+
+            int rotationVal = line.StartsWith("L") ? -val : val;
+
+            int temp = currentPosition + rotationVal;
+            Console.WriteLine($"{line} {currentPosition} + {rotationVal} = {temp}");
+
+            currentPosition = temp % 100;
+            if (currentPosition < 0)
+            {
+                currentPosition += 100;
+                Console.WriteLine($"\t({temp} mod 100) + 100 = {currentPosition}");
+            }
+            else
+            {
+                Console.WriteLine($"\t{temp} mod 100 = {currentPosition}");
+            }
+
+            if (currentPosition == 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine($"\nAnswer: {count}");
+    }
+}
